b033ba18c693461a7ceba13522bad08aeab079f9
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2025-06-08 13:01:08 +0300

    scaffold quiz controller and routes

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    public function index()
+    {
+        return view("welcome");
+    }
+
+    public function start()
+    {
+
+    }
+
+    public function quiz()
+    {
+
+    }
+
+    public function results()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\QuizController::class, 'index']);
+Route::post('/start-quiz',[App\Http\Controllers\QuizController::class, 'start'])->name('start-quiz');
+Route::get('/quiz',[App\Http\Controllers\QuizController::class, 'quiz'])->name('quiz');
+Route::get('/results',[App\Http\Controllers\QuizController::class, 'results'])->name('results');
