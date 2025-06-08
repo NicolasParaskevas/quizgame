a58f245b34644d1d12723c6b6bae4642cd819cf0
Nicolas Paraskevas <nicolasparaskevas@hotmail.com>
2025-06-08 21:48:28 +0300

    a little space

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 Route::post('/start-quiz',[App\Http\Controllers\HomeController::class, 'start'])->name('start-quiz');
+
 Route::post('/answer',[App\Http\Controllers\QuizController::class, 'answer'])->name('answer');
 Route::get('/quiz/{$index}',[App\Http\Controllers\QuizController::class, 'quiz'])->name('quiz');
 Route::get('/results',[App\Http\Controllers\QuizController::class, 'results'])->name('results');
